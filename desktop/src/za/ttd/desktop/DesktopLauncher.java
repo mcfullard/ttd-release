894fecdaa2c3e156b1ada91fccfc92239ac3bb19
unobtruse <unobtruse@gmail.com>
2015-07-17 13:45:28 +0200

    added automatic texture packing on game launch. this should be removed once in release

--- desktop/src/za/ttd/desktop/DesktopLauncher.java
+++ desktop/src/za/ttd/desktop/DesktopLauncher.java
@@ -2,10 +2,14 @@ package za.ttd.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import za.ttd.ttd;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		// this line should only be used during development - remove on release
+		TexturePacker.process("core/assets/sprites/in", "core/assets/sprites/out", "sprites");
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new ttd(), config);
 	}
