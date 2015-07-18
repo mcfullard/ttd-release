abb3f65652a0b5e4bb482c94c071b680cbe88623
unobtruse <unobtruse@gmail.com>
2015-07-18 22:35:31 +0200

    restructured directories

--- desktop/src/za/ttd/desktop/DesktopLauncher.java
+++ desktop/src/za/ttd/desktop/DesktopLauncher.java
@@ -2,14 +2,14 @@ package za.ttd.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.hiero.Hiero;
 import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import za.ttd.ttd;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		// this line should only be used during development - remove on release
-		TexturePacker.process("core/assets/img/sprites/in", "core/assets/img/sprites/out", "sprites");
-
+		// this code should only run used during development - remove on release
+		TexturePacker.process("core/assets/textures/in", "core/assets/textures/out", "texture");
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = ttd.WIDTH;
 		config.height = ttd.HEIGHT;
