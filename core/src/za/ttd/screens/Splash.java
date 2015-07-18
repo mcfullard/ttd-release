b9a581ee2f4a0d715c999f429262319f7ae93bf3
unobtruse <unobtruse@gmail.com>
2015-07-18 21:59:55 +0200

    added splash screen

--- core/src/za/ttd/screens/Splash.java
+++ core/src/za/ttd/screens/Splash.java
@@ -1,6 +1,13 @@
 package za.ttd.screens;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 /**
  * @author minnaar
@@ -10,15 +17,29 @@ import com.badlogic.gdx.Screen;
  *
  */
 public class Splash implements Screen {
+    private Texture texture = new Texture(Gdx.files.internal("core/assets/img/screens/splash.png"));
+    private Image splashImage = new Image(texture);
+    private Stage stage = new Stage();
 
     @Override
     public void show() {
-
+        stage.addActor(splashImage);
+        splashImage.addAction(Actions.sequence(
+                Actions.alpha(0f),
+                Actions.fadeIn(0.5f),
+                Actions.delay(2),
+                Actions.run(() -> {
+                    ((Game)Gdx.app.getApplicationListener()).setScreen(new Splash());
+                })
+        ));
     }
 
     @Override
     public void render(float delta) {
-
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act();
+        stage.draw();
     }
 
     @Override
@@ -38,11 +59,12 @@ public class Splash implements Screen {
 
     @Override
     public void hide() {
-
+        dispose();
     }
 
     @Override
     public void dispose() {
-
+        texture.dispose();
+        stage.dispose();
     }
 }
