c8f7338ec24dd8857e8def3aa83adb961df4c0dc
unobtruse <unobtruse@gmail.com>
2015-07-18 13:43:09 +0200

    basic animation libgdx tut 3b

--- core/src/za/ttd/ttd.java
+++ core/src/za/ttd/ttd.java
@@ -2,26 +2,87 @@ package za.ttd;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Timer;
 
-public class ttd extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class ttd extends ApplicationAdapter implements InputProcessor {
+	private SpriteBatch batch;
+	private TextureAtlas textureAtlas;
+	private Animation animation;
+	private float elapsedTime = 0;
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		textureAtlas = new TextureAtlas(Gdx.files.internal(
+				"core/assets/sprites/out/sprites.atlas"
+		));
+		animation = new Animation(
+				1/10f,
+				textureAtlas.findRegion("characters/decayLeft"),
+				textureAtlas.findRegion("characters/decayFull"),
+				textureAtlas.findRegion("characters/decayRight")
+		);
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	public void dispose() {
+		batch.dispose();
+		textureAtlas.dispose();
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl20.glClearColor(0,0,0,1);
+		Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		elapsedTime += Gdx.graphics.getDeltaTime();
+		batch.draw(animation.getKeyFrame(elapsedTime, true), 0,0);
 		batch.end();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
