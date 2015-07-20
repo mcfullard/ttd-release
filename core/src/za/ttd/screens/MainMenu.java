376c55c1e43825574150190ce69c99befb516939
unobtruse <unobtruse@gmail.com>
2015-07-20 21:10:10 +0200

    removed dependency

--- core/src/za/ttd/screens/MainMenu.java
+++ core/src/za/ttd/screens/MainMenu.java
@@ -2,7 +2,6 @@ package za.ttd.screens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -39,7 +38,7 @@ public class MainMenu extends AbstractScreen {
         buttonContinue.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                ((Game)Gdx.app.getApplicationListener()).setScreen(new Splash(game));
+                game.setScreen(new SplashScreen(game));
             }
         });
 
