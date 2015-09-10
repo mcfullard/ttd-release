f4232f6210d4f5f243bfbc83c58633fb0c11eb11
unobtruse <unobtruse@gmail.com>
2015-09-11 00:01:39 +0200

    added methods to determine whether a given coordinate had adjacent paths.

--- core/src/za/ttd/mapgen/Map.java
+++ core/src/za/ttd/mapgen/Map.java
@@ -1,5 +1,6 @@
 package za.ttd.mapgen;
 
+
 import static java.lang.System.arraycopy;
 
 /**
@@ -20,8 +21,73 @@ public class Map {
 
     public int[][] getMap() { return map; }
 
-    public boolean isWall(int x, int y) {
-        return map[y][x] == WALL;
+    /**
+     * @param c Column or positive X
+     * @param r Row or positive Y
+     * @return True if there is a wall at the respective coordinate
+     */
+    public boolean isWall(int c, int r) {
+        return map[r][c] == WALL;
+    }
+
+    /**
+     * @param c Column or positive X
+     * @param r Row or positive Y
+     * @return True if path to the left of coordinates
+     */
+    public boolean pathLeft(int c, int r) {
+        if(withinBounds(c - 1, r))
+            if(!isWall(c - 1, r))
+                return true;
+        return false;
+    }
+
+    /**
+     * @param c Column or positive X
+     * @param r Row or positive Y
+     * @return True if path to the right of coordinates
+     */
+    public boolean pathRight(int c, int r) {
+        if(withinBounds(c + 1, r))
+            if(!isWall(c + 1, r))
+                return true;
+        return false;
+    }
+
+    /**
+     * @param c Column or positive X
+     * @param r Row or positive Y
+     * @return True if path above coordinates
+     */
+    public boolean pathUp(int c, int r) {
+        if(withinBounds(c, r - 1))
+            if(!isWall(c, r - 1))
+                return true;
+        return false;
+    }
+
+    /**
+     * @param c Column or positive X
+     * @param r Row or positive Y
+     * @return True if path below coordinates
+     */
+    public boolean pathDown(int c, int r) {
+        if(withinBounds(c, r + 1))
+            if(!isWall(c, r + 1))
+                return true;
+        return false;
+    }
+
+    /**
+     * @param c Column or positive X
+     * @param r Row or positive Y
+     * @return True if coordinate is on the map
+     */
+    public boolean withinBounds(int c, int r) {
+        return r < map.length &&
+                r >= 0 &&
+                c < map[0].length &&
+                c >= 0;
     }
 
     public void verticalMirror() {
@@ -129,4 +195,5 @@ public class Map {
         }
         map = newMap;
     }
+
 }
