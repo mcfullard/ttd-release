8daf5b1fbc20c5c4e264e61a3003ef683cdab800
unobtruse <unobtruse@gmail.com>
2015-09-12 17:05:22 +0200

    Added an exception as part of the debugging process. Added getters and setters too.

--- core/src/za/ttd/pathfinding/Edge.java
+++ core/src/za/ttd/pathfinding/Edge.java
@@ -1,15 +1,14 @@
 package za.ttd.pathfinding;
-import za.ttd.characters.objects.Position;
 
-import java.util.LinkedList;
-import java.util.List;
+import za.ttd.characters.objects.Position;
+import za.ttd.exceptions.NodeNotIncidentOnEdgeException;
 
 /**
  * @author minnaar
  * @since 2015/09/09.
  */
 public class Edge {
-    private int weight = 1;
+    private Integer weight = 1;
     private Node source;
     private Node destination;
     public Edge(Node source, Node destination) {
@@ -17,10 +16,58 @@ public class Edge {
         this.destination = destination;
     }
 
-    public List<Node> getNodes() {
-        List<Node> nodes = new LinkedList<>();
-        nodes.add(source);
-        nodes.add(destination);
-        return nodes;
+    public Node getAdjacent(Node node) {
+        if(node.getOrigin().equals(source.getOrigin()))
+            return destination;
+        if(node.getOrigin().equals(destination.getOrigin()))
+            return source;
+        throw new NodeNotIncidentOnEdgeException(String.format(
+                "%s is not incident on the edge %s",
+                node.toString(),
+                this.toString()
+        ));
+    }
+
+    public void setAdjacent(Node node, Node newAdjacent) {
+        if(node.getOrigin().equals(source.getOrigin())) {
+            destination = newAdjacent;
+        } else if(node.getOrigin().equals(destination.getOrigin())) {
+            source = newAdjacent;
+        } else {
+            throw new NodeNotIncidentOnEdgeException(String.format(
+                    "%s is not incident on the edge %s",
+                    node.toString(),
+                    this.toString()
+            ));
+        }
+    }
+
+    public Position getAdjacentPosition(Node node) {
+        Node adjacent = getAdjacent(node);
+        if(adjacent != null)
+            return adjacent.getOrigin();
+        return null;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public Edge clone() {
+        Edge clone = new Edge(source, destination);
+        clone.setWeight(weight);
+        return clone;
+    }
+
+    public Node getSource() {
+        return source;
+    }
+
+    public Node getDestination() {
+        return destination;
     }
 }
