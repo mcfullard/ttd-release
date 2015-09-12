bcfacf291c45947cb858337a64d43d1e53b74dfa
unobtruse <unobtruse@gmail.com>
2015-09-12 17:06:13 +0200

    Changed int to Integer to support use of MAX_VALUE as a theoretical "infinity"

--- core/src/za/ttd/pathfinding/EdgeContainer.java
+++ core/src/za/ttd/pathfinding/EdgeContainer.java
@@ -5,9 +5,9 @@ package za.ttd.pathfinding;
  * @since 2015/09/09.
  */
 public class EdgeContainer {
-    private Edge edge;
-    private int distance;
-    public EdgeContainer(Edge edge, int distance) {
+    public Edge edge;
+    public Integer distance;
+    public EdgeContainer(Edge edge, Integer distance) {
         this.edge = edge;
         this.distance = distance;
     }
