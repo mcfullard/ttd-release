b84f15cb36b4c360bb19f6272c4248f13a15a528
unobtruse <unobtruse@gmail.com>
2015-09-15 22:57:12 +0200

    created a large number as the default "infinity"

--- core/src/za/ttd/pathfinding/EdgeContainer.java
+++ core/src/za/ttd/pathfinding/EdgeContainer.java
@@ -5,6 +5,7 @@ package za.ttd.pathfinding;
  * @since 2015/09/09.
  */
 public class EdgeContainer {
+    public static final int LARGE = 10000;
     public Edge edge;
     public Integer distance;
     public EdgeContainer(Edge edge, Integer distance) {
