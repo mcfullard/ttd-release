3af7a23f226e04fc0acb4a82298c233dc66e26b4
unobtruse <unobtruse@gmail.com>
2015-09-16 21:04:12 +0200

    Revert "handled exceptions accordingly. DB failure should not kill the application: it should atleast output something though. Consider adding support for a log file (Gdx probably has this somewhere)."
    This reverts commit 09c224772750aa34cfbddb05e412d8aa38b30386.
    
    Reason: Exceptions should be handled by the class using the db methods. e.g.: using local storage on db connect failure.

--- core/src/za/ttd/database/ConnectDB.java
+++ core/src/za/ttd/database/ConnectDB.java
@@ -1,7 +1,5 @@
 package za.ttd.database;
 
-import com.badlogic.gdx.Gdx;
-
 import java.net.URISyntaxException;
 import java.sql.*;
 
@@ -187,28 +185,17 @@ public class ConnectDB {
      * @return true if connection to database is possible , false if not.
      * @throws Exception
      */
-    public static boolean TestConnectivity()
+    public static boolean TestConnectivity() throws Exception
     {
+        Class.forName("org.postgresql.Driver");
         try {
-            Class.forName("org.postgresql.Driver");
             Connection connection = getConnection();
             return true;
             //reachable= connection.isValid(10);
         }
         catch (SQLException e)
         {
-            Gdx.app.log("DATABASE ERROR", e.getMessage());
-        } catch (ClassNotFoundException e) {
-            Gdx.app.log("DATABASE ERROR", String.format("%s. %s",
-                    e.getMessage(),
-                    "It could be that the Postgres drivers are not installed."
-            ));
-        } catch (URISyntaxException e) {
-            Gdx.app.log("DATABASE ERROR",String.format("%s. %s",
-                    e.getMessage(),
-                    "Try checking the connection string."
-            ));
+            return false;
         }
-        return false;
     }
 }
