deaf1b9fd87daa66e44c0ef159ae8599dceaddee
unobtruse <unobtruse@gmail.com>
2015-09-20 13:46:18 +0200

    added new message types

--- core/src/za/ttd/characters/states/MessageType.java
+++ core/src/za/ttd/characters/states/MessageType.java
@@ -12,4 +12,8 @@ public class MessageType {
     public static final int MOUTHWASH_COLLECTED = 4;
     public static final int TOOTHBRUSH_COLLECTED = 5;
     public static final int PLAQUE_COLLECTED = 6;
+    public static final int BROADCAST_ENEMIES = 7;
+    public static final int LEVEL_LOADING = 8;
+    public static final int MOUTHWASH_EXPIRED = 9;
+    public static final int BADBREATH_DEAD = 10;
 }
