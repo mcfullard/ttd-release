075d872fdd038760ecfbcd767487e0a2ca2f738a
unobtruse <unobtruse@gmail.com>
2015-09-27 20:52:03 +0200

    changed logic of user input screen

--- core/src/za/ttd/screens/UserInputScreen.java
+++ core/src/za/ttd/screens/UserInputScreen.java
@@ -2,6 +2,7 @@ package za.ttd.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -9,7 +10,11 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import za.ttd.game.Game;
+import za.ttd.game.Level;
+import za.ttd.game.Player;
 
 /**
  * @author minnaar
@@ -22,7 +27,7 @@ public class UserInputScreen extends AbstractScreen {
     private Label labelName = new Label("Enter Name", skin);
     private TextField textName = new TextField("", skin);
     private TextButton buttonContinue = new TextButton("Continue", skin);
-    private Dialog dialog = new Dialog("Info", skin);
+    private Dialog dialog;
 
     public UserInputScreen(Game game) {
         super(game);
@@ -30,6 +35,22 @@ public class UserInputScreen extends AbstractScreen {
 
     @Override
     public void show() {
+        Drawable tfBack = textName.getStyle().background;
+        tfBack.setLeftWidth(tfBack.getLeftWidth() + 10);
+        textName.setTextFieldListener(new TextField.TextFieldListener() {
+            @Override
+            public void keyTyped(TextField textField, char c) {
+                if (c == '\r' || c == '\n') { // when the user presses enter
+                    validateInput(textName.getText());
+                }
+            }
+        });
+        buttonContinue.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                validateInput(textName.getText());
+            }
+        });
         table.add(labelName).padBottom(20).row();
         table.add(textName).size(282,54).padBottom(20).row();
         table.add(buttonContinue).padBottom(20).row();
@@ -52,5 +73,37 @@ public class UserInputScreen extends AbstractScreen {
         skin.dispose();
     }
 
+    private void validateInput(String name) {
+        if (!name.isEmpty()) {
+            Player loadedPlayer = game.loadPlayer(name);
+            setupDialog();
+            if (loadedPlayer == null) {
+                loadedPlayer = new Player(name, 0, 1, 2);
+                game.setPlayer(loadedPlayer);
+                dialog.show(stage);
+            } else {
+                game.setPlayer(loadedPlayer);
+                toMainMenu();
+            }
+        }
+    }
 
+    private void setupDialog() {
+        Skin default_skin = new Skin(Gdx.files.internal("core/assets/defaultui/uiskin.json"));
+        dialog = new Dialog("Confirm Player", default_skin) {
+            @Override
+            public void result(Object obj) {
+                if ((boolean) obj) {
+                    toMainMenu();
+                }
+            }
+        };
+        dialog.text("No player with this name was found. Would you like to create one?");
+        dialog.button("No", false);
+        dialog.button("Yes", true);
+    }
+
+    private void toMainMenu() {
+        game.setScreen(new MainMenu(game));
+    }
 }
