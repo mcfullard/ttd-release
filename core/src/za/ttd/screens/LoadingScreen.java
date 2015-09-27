599684e9b194d099bda6c571f313a8a711aac12b
RCB <rhysbotes@gmail.com>
2015-09-27 22:01:06 +0200

    loadingscreen skeleton

---
+++ core/src/za/ttd/screens/LoadingScreen.java
@@ -0,0 +1,17 @@
+package za.ttd.screens;
+
+import za.ttd.ttd;
+
+/**
+ * Created by RCB on 2015/09/18.
+ */
+public class LoadingScreen extends AbstractScreen {
+    public LoadingScreen(ttd game) {
+        super(game);
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+}
