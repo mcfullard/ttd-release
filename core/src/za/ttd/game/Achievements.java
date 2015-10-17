0643d8729fea0a089b9328527a442a76e9da3995
Bas <bas24.7live@gmail.com>
2015-10-17 15:14:15 +0200

    Achievements class complete. Added an achievement data structure.

--- core/src/za/ttd/game/Achievements.java
+++ core/src/za/ttd/game/Achievements.java
@@ -3,32 +3,73 @@ package za.ttd.game;
 import java.util.ArrayList;
 
 public class Achievements {
-
     private Player player;
-    private ArrayList<String> achievements;
+    private ArrayList<Achievement> achievementsObtained, achievements;
 
     public Achievements() {
         player = Player.getInstance();
+        achievementsObtained = new ArrayList<>();
         achievements = new ArrayList<>();
+
     }
 
     public void updateAchievements() {
-        if (player.scoring.getPowersUsed() < 0)
-            achievements.add("Man on the move: Finished Level without using a minty mouthwash");
+        for (Achievement achievement:achievements) {
+            int metric = 0;
+            boolean check = true;
+            switch (achievement.getMetric()) {
+                case "LIVES":
+                    metric = player.getLives();
+                    break;
+                case "HIGHSCORE":
+                    metric = player.getHighestScore();
+                    break;
+                case "BADBREATH_KILLED":
+                    metric = player.scoring.getTotBadBreathKilled();
+                    break;
+                case "LEVEL":
+                    metric = player.getHighestLevel();
+                    break;
+                default:
+                    check = false;
+                    break;
+            }
 
-        if (player.scoring.getTotBadBreathKilled() > 50)
-            achievements.add("Bad Breath Master: Killed at 50 Bad Breath");
-
-        if (player.scoring.getTotCollectiblesFound() > 10000)
-            achievements.add("Plaque Master: Collected 10000 Plaque items");
+            if (check && doComparison(metric, achievement.getCondition(), achievement.getBound()))
+                obtainedAchievement(achievement);
+        }
     }
 
-    public ArrayList<String> getAchievements() {
-        return achievements;
+
+    public boolean doComparison(int metric,String condition, int bound){
+        switch (condition) {
+            case "<":
+                return metric < bound;
+            case "==":
+                return metric == bound;
+            case ">":
+                return metric > bound;
+            case "<=":
+                return metric <= bound;
+            case ">=":
+                return metric >= bound;
+            default:
+                return false;
+        }
     }
 
+    private void obtainedAchievement(Achievement achievement) {
+        boolean obtained = false;
+        for (int i = 0; i < achievementsObtained.size(); i++)
+            if (achievement.getId() == achievementsObtained.get(i).getId())
+                obtained = true;
 
-    public void clearAchievements() {
-        achievements.clear();
+        if (!obtained)
+            achievementsObtained.add(achievement);
+
+    }
+
+    public ArrayList<Achievement> getAchievements() {
+        return achievementsObtained;
     }
 }
