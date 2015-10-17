b5a2628a3cfaa13f8481c97d8ff6e966463a16db
unobtruse <unobtruse@gmail.com>
2015-10-17 21:18:24 +0200

    no longer packing on run (change if you decide to change the textures)

--- desktop/src/za/ttd/desktop/DesktopLauncher.java
+++ desktop/src/za/ttd/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ import za.ttd.game.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		// this code should only run used during development - remove on release
-		TexturePacker.process("core/assets/textures/in", "core/assets/textures/out", "texture");
+		//TexturePacker.process("core/assets/textures/in", "core/assets/textures/out", "texture");
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = Game.WIDTH;
 		config.height = Game.HEIGHT;
