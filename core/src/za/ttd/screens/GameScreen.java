c179dba06379effaefaf22df0d485d121be882dd
mcfullard <mcfullard@gmail.com>
2015-10-19 01:37:15 +0200

    working on instructions dialog

--- core/src/za/ttd/screens/GameScreen.java
+++ core/src/za/ttd/screens/GameScreen.java
@@ -4,12 +4,18 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ai.msg.MessageManager;
 import com.badlogic.gdx.ai.msg.Telegram;
 import com.badlogic.gdx.ai.msg.Telegraph;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import za.ttd.characters.states.MessageType;
+import za.ttd.database.ConnectDB;
 import za.ttd.game.Game;
 import za.ttd.game.Level;
+import za.ttd.game.Player;
 
 public class GameScreen extends AbstractScreen implements Telegraph{
 
@@ -32,10 +38,19 @@ public class GameScreen extends AbstractScreen implements Telegraph{
     @Override
     public void show() {
         super.show();
+        TextButton buttonOkay = new TextButton("Okay", skin);
+        buttonOkay.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                dialog.hide();
+            }
+        });
         dialog = new Dialog("Instructions", skin);
         dialog.text("Lorem ipsum dolar sit amet. Consecutor... I can't remember the rest");
-        dialog.button("Okay");
-        dialog.show(stage);
+        dialog.button(buttonOkay);
+        if(ConnectDB.checkPlayerExists(Player.getInstance().getName())) {
+            dialog.show(stage);
+        }
     }
 
     @Override
