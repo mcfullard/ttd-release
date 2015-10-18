f14fd7777b28ede4168e737e53107a9fd032948d
Bas24live <bas24.7live@gmail.com>
2015-10-19 01:54:05 +0200

    Dialog added to new games

--- core/src/za/ttd/screens/GameScreen.java
+++ core/src/za/ttd/screens/GameScreen.java
@@ -4,13 +4,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ai.msg.MessageManager;
 import com.badlogic.gdx.ai.msg.Telegram;
 import com.badlogic.gdx.ai.msg.Telegraph;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import za.ttd.characters.states.MessageType;
 import za.ttd.database.ConnectDB;
 import za.ttd.game.Game;
@@ -38,27 +34,20 @@ public class GameScreen extends AbstractScreen implements Telegraph{
     @Override
     public void show() {
         super.show();
-        TextButton buttonOkay = new TextButton("Okay", skin);
-        buttonOkay.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                dialog.hide();
-            }
-        });
         dialog = new Dialog("Instructions", skin);
         dialog.text("Lorem ipsum dolar sit amet. Consecutor... I can't remember the rest");
-        dialog.button(buttonOkay);
         if(ConnectDB.checkPlayerExists(Player.getInstance().getName())) {
             dialog.show(stage);
         }
+        dialog.button("Okay");
     }
 
     @Override
     public void render(float delta) {
         level.render();
-
         stage.act();
         stage.draw();
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
