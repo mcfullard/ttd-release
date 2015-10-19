c0d66ee3bcdbcf2dc15cf5f81ba1c3c80a238697
mcfullard <mcfullard@gmail.com>
2015-10-19 23:17:02 +0200

    needed for debug

--- desktop/src/za/ttd/desktop/DesktopLauncher.java
+++ desktop/src/za/ttd/desktop/DesktopLauncher.java
@@ -9,7 +9,7 @@ import za.ttd.game.Preferences;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		//TexturePacker.process("textures/in", "textures/out", "texture");
+		TexturePacker.process("textures/in", "textures/out", "texture");
 		System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true");
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = Game.WIDTH;
